a316b1c518bfd53d68777b036d45365bb04465e7
lord.jon.of.breen@gmail.com <lord.jon.of.breen@gmail.com@33954190-f561-50f0-de75-344a2561cf6e>
2011-10-05 03:56:11 +0000

    Added IO initialization. Added global IO names. Added more global settings variables.

--- trunk/versa-step/main.h
+++ trunk/versa-step/main.h
@@ -1,3 +1,5 @@
+#ifndef __MAIN_H
+#define __MAIN_H
 /*----------------------------------------------------------------------------+
 |  Project:  Versa Step                                                       |
 |  File:  main.h                                                              |
@@ -22,12 +24,12 @@
 
 // global settings variables
 char curMode = 0;  //the current mode the VersaStep is in (use MODE enum)
-const char BIPOLAR = 0;		//for driving a bipolar stepper motor
-const char UNIPOLAR = 1;	//for driving a unipolar stepper motor
-const char DUAL_H_I = 2;	//2 independent H-bridges in current mode
-const char DUAL_H_V = 3;	//2 independent H-bridges in voltage mode
-const char QUAD_LSD_I = 4;	//4 independent low-side drivers in current mode
-const char QUAD_LSD_V = 5;	//4 independent low-side drivers in voltage mode
+#define BIPOLAR		0	//for driving a bipolar stepper motor
+#define UNIPOLAR	1	//for driving a unipolar stepper motor
+#define DUAL_H_I	2	//2 independent H-bridges in current mode
+#define DUAL_H_V	3	//2 independent H-bridges in voltage mode
+#define QUAD_LSD_I	4	//4 independent low-side drivers in current mode
+#define QUAD_LSD_V	5	//4 independent low-side drivers in voltage mode
 
 unsigned short faultCurLimit = 12000;  //(mA) maximum current for any channel in any mode 
 									   //this is intended to be hardware protection and will
@@ -49,10 +51,37 @@ unsigned short curLimit4 = 2100;  //(mA) bipolar or unipolar - not used
 								  //dual H-bridge - not used
 								  //quad LSD - max current for B2
 
+// current decay mode
+char iDecay = 0;  //the current decay mode in use
+#define FAST_DECAY	0		//current is pushed up and pulled down
+#define SLOW_DECAY	1		//current is pushed up and "freewheeled" down
+
+// microstepping modes
+char uStep = 0;  //the microstepping mode in use
+#define USTEP2		2		//(1/2) stepping
+#define USTEP4		4		//(1/4) stepping
+#define USTEP8		8		//(1/8) stepping
+#define USTEP16		16		//(1/16) stepping
+#define USTEP32		32		//(1/32) stepping
+#define USTEP64		64		//(1/64) stepping
+
+
+
 // motor physical parameters (used for control parameter estimation)
 long R_load = 1000;  //(mOhms) the winding resistance of the motor or load
 int L_load = 100;  //(uH) the winding inductance of the motor or load
 
 
+// IO names
+#define DIR @RA6
+#define STEP @RB4
+#define STATUS_LED @LATA7
+#define OVERLOAD_LED @LATB3
+
+
+
 // function prototypes
 void main(void);
+
+
+#endif
